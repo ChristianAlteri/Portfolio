344623e97244aece05f2eb8a25dd6e8669d53422
ChristianAlteri <alteri.christian@gmail.com>
2023-08-04 12:39:03 +0800

    passing children corretly though the layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import "./Layout.css"; 
 import Contact from "../pages/Contact";
+import { Navbar } from "react-bootstrap";
 
-const Layout = () => {
+const Layout = ({children}) => {
   return (
     <div>
       <section>
-        <Contact />
+        {children}
         <div className="air air1"></div>
         <div className="air air2"></div>
         <div className="air air3"></div>
