f58d3f0d901d418705574c9459739a0edf258e52
ChristianAlteri <alteri.christian@gmail.com>
2023-08-04 11:54:16 +0800

    trying to push pages through the layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,17 +1,19 @@
+import React from "react";
 import "./Layout.css"; 
+import Contact from "../pages/Contact";
 
 const Layout = () => {
-    return ( 
-        <div>
-            hiiii
-            <section>
-                <div class='air air1'></div>
-                <div class='air air2'></div>
-                <div class='air air3'></div>
-                <div class='air air4'></div>
-            </section>
-        </div>
-     );
-}
- 
+  return (
+    <div>
+      <section>
+        <Contact />
+        <div className="air air1"></div>
+        <div className="air air2"></div>
+        <div className="air air3"></div>
+        <div className="air air4"></div>
+      </section>
+    </div>
+  );
+};
+
 export default Layout;
